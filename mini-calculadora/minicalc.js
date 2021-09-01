b5a03f41d2c1a58c27af4cc7d4395131861de0be
williamsilveiraf <williamsilveiraf@hotmail.com>
2021-09-01 12:34:48 -0300

    mini calc

---
+++ mini-calculadora/minicalc.js
@@ -0,0 +1,12 @@
+const n1 = Number(window.prompt('Digite um número'))
+const n2 = Number(window.prompt('Digite outro número'))
+
+const mais = n1 + n2
+const menos = n1 - n2
+const vezes = n1 * n2
+const dividido = n1 / n2
+
+window.alert("o resultado da soma é " + mais )
+window.alert("O resultado da subtração é " + menos)
+window.alert("O resultado  da multiplicação é " + vezes)
+window.alert("O resultado da divisão é " + dividido)
