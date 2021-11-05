2b7d5c61a5eee05461f4071991143bdb003585d3
William Silveira <williamsilveiraf@hotmail.com>
2021-11-05 17:01:33 -0300

    update

---
+++ React_Framework/composeReactComponents.js
@@ -0,0 +1,60 @@
+const NonCitrus = () => {
+  return (
+    <div>
+      <h3>Non-Citrus:</h3>
+      <ul>
+        <li>Apples</li>
+        <li>Blueberries</li>
+        <li>Strawberries</li>
+        <li>Bananas</li>
+      </ul>
+    </div>
+  )
+}
+const Citrus = () => {
+  return (
+    <div>
+      <h3>Citrus:</h3>
+      <ul>
+        <li>Lemon</li>
+        <li>Lime</li>
+        <li>Orange</li>
+        <li>Grapefruit</li>
+      </ul>
+    </div>
+  )
+}
+
+class Fruits extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h2>Fruits:</h2>
+          { /* Change code below this line */ }
+          <NonCitrus />
+          <Citrus />
+          { /* Change code above this line */ }
+        </div>
+      );
+    }
+  };
+  
+class TypesOfFood extends React.Component {
+    constructor(props) {
+       super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h1>Types of Food:</h1>
+          { /* Change code below this line */ }
+          <Fruits />
+          { /* Change code above this line */ }
+          <Vegetables />
+        </div>
+      );
+    }
+};
