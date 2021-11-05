29750ebbc7a4690602267c076faa9fce7be19972
William Silveira <williamsilveiraf@hotmail.com>
2021-11-05 11:18:14 -0300

    update of files

---
+++ React_Framework/createAStatelessFunctionalComponent.js
@@ -0,0 +1,9 @@
+const MyComponent = function() {
+    // Change code below this line
+    return (
+            <div>
+              <p>test</p>
+            </div>
+            )
+    // Change code above this line
+  }
