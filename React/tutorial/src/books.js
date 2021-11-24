4d4daa4b2e0045d000aab163a429322f163a5aa7
William Silveira <williamsilveiraf@hotmail.com>
2021-11-24 20:15:52 -0300

    react progress

---
+++ React/tutorial/src/books.js
@@ -0,0 +1,20 @@
+export const data = [
+    { 
+     id: 1,
+      img: 'https://images-na.ssl-images-amazon.com/images/I/817VlxnFeRL._AC_UL127_SR127,127_.jpg',
+      title:'Betrayal: The Final Act of the Trump Show',
+      author: 'Jonathan Karl'
+    },
+    { 
+      id: 2,
+      img: 'https://images-na.ssl-images-amazon.com/images/I/81QF6ehEwCL._AC_UL127_SR127,127_.jpg',
+      title:'At First Light (Dr. Evan Wilding Book)',
+      author: 'Barbara Nickless'
+    },
+    {
+      id: 3,
+      img: 'https://images-na.ssl-images-amazon.com/images/I/81GeAcdMCsL._AC_UL127_SR127,127_.jpg',
+      title:'Humans',
+      author: 'Brandon Stanton'
+    }
+];
